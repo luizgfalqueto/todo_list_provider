9cd9c3dcf341d012df78d7ce4c7ca8c25d790c0b
Luiz Falqueto <luiz@quant1.com.br>
2024-04-25 22:23:50 -0300

    feat: auth module with login page ui

---
+++ lib/app/core/widget/todo_list_logo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/ui/them_extensions.dart';
+
+class TodoListLogo extends StatelessWidget {
+  const TodoListLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'assets/images/logo.png',
+          height: 200,
+        ),
+        Text(
+          'Todo List',
+          style: context.textTheme.titleLarge,
+        )
+      ],
+    );
+  }
+}
